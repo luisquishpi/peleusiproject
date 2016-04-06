7001d055b1f74b8e4b77373b79b71f35018d42fb
Luisa Carangui <lmccarangui@gmail.com>
2016-04-05 20:45:59 -0500

    Creado entidad y dao de UnidadMedida

---
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/UnidadMedidaDao.java
@@ -0,0 +1,11 @@
+package ec.peleusi.models.daos;
+
+import ec.peleusi.models.entities.UnidadMedida;
+
+public class UnidadMedidaDao extends GenericDao<UnidadMedida, Integer> {
+
+	public UnidadMedidaDao() {
+		super(UnidadMedida.class);
+
+	}
+}
---
+++ peleusiproject/src/main/java/ec/peleusi/models/entities/UnidadMedida.java
@@ -0,0 +1,70 @@
+package ec.peleusi.models.entities;
+
+import javax.persistence.Entity;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "unidamedida")
+
+public class UnidadMedida {
+	
+		public UnidadMedida()
+		{
+		
+		}
+	
+		public UnidadMedida(String nombre, String abreviatura)
+		{
+			super();
+			this.id = null;
+			this.nombre = nombre;
+			this.abreviatura = abreviatura;
+		}
+		
+	@Id
+	@GeneratedValue(strategy=IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+	
+	@Column(name = "nombre", nullable = false, length = 50)
+	private String nombre;
+	
+	@Column(name = "abreviatura", nullable = false, length = 10)
+	private String abreviatura;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getAbreviatura(){
+		return abreviatura;		
+	}
+	
+	public void setAbreviatura(String abreviatura){		
+		this.abreviatura = abreviatura;
+	}
+	
+	@Override
+	public String toString()
+		{
+			return "[id=" + id + ", nombre=" + nombre + ", abreviatura=" + abreviatura +"]";			
+		}	
+	
+}
