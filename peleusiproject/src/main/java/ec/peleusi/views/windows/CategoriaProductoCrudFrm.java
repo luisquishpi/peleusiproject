7ca08f33f7972397e0906ba005d8c006d7e44691
Celia <Celia@Celia-PC>
2016-04-06 19:21:32 -0500

    Concluido historia CategoriaProducto

---
+++ peleusiproject/src/main/java/ec/peleusi/views/windows/CategoriaProductoCrudFrm.java
@@ -0,0 +1,293 @@
+package ec.peleusi.views.windows;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import ec.peleusi.models.entities.CategoriaProducto;
+import ec.peleusi.controllers.CategoriaProductoController;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class CategoriaProductoCrudFrm extends JInternalFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTree tree;
+	private DefaultTreeModel modelo;
+	private JLabel lblId;
+	private JLabel lblDepen;
+	private JCheckBox chbxContieneProductos;
+	private JButton btnNuevo;
+	private JLabel lblNombre;
+	private JLabel lblDependencia;
+	private JButton btnGuardar;
+	private JPanel panel;
+	private JButton btnCancelar;
+	private JScrollPane scrollPane;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JTextField txtNombre;
+	private JTextField txtDependencia;
+	private ec.peleusi.models.entities.CategoriaProducto categoriaProducto;
+
+	public CategoriaProductoCrudFrm() {
+		setIconifiable(true);
+		setClosable(true);
+		setTitle("Categorías de Productos");
+		crearControles();
+		crearEventos();
+		cargarArbolCategoriaProductos();
+
+	}
+
+	public void cargarArbolCategoriaProductos() {
+		DefaultMutableTreeNode raiz = new DefaultMutableTreeNode("Categorias");
+		modelo = new DefaultTreeModel(raiz);
+		getHojas(raiz, "0");
+		tree.setModel(modelo);
+	}
+
+	public void getHojas(DefaultMutableTreeNode raiz, String id) {
+		CategoriaProductoController categoriaProductoController = new CategoriaProductoController();
+		List<CategoriaProducto> lista;
+		lista = categoriaProductoController.CategoriaProductoList(Integer.parseInt(id));
+		for (int i = 0; i < lista.size(); i++) {
+			DefaultMutableTreeNode hoja = new DefaultMutableTreeNode();
+			hoja.setUserObject(lista.get(i));
+			getHojas(hoja, lista.get(i).getId().toString());
+			raiz.add(hoja);
+		}
+	}
+
+	private void limpiarcampos() {
+
+		lblId.setText("0");
+		txtNombre.setText("");
+		chbxContieneProductos.setSelected(false);
+		chbxContieneProductos.setEnabled(true);		
+		txtNombre.requestFocus();
+
+	}
+
+	private boolean isCamposLlenos() {
+		boolean llenos = true;
+		if (txtNombre.getText().isEmpty() || lblDependencia.getText().isEmpty())
+			llenos = false;
+		return llenos;
+	}
+
+	private void verificarUbicacionNodoNuevo() {
+		DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+		if (nodo.getLevel() == 0) {
+			lblDepen.setText("0");
+			txtDependencia.setText("Categorias");
+		} else {
+			lblDepen.setText(lblId.getText());
+			txtDependencia.setText(txtNombre.getText());
+		}
+	}
+
+	private void agregarNodoArbol(CategoriaProducto categoriaProducto) {
+		DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+		DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+		model.insertNodeInto(new DefaultMutableTreeNode(categoriaProducto), nodo, model.getChildCount(nodo));
+
+	}
+
+	private void actualizarNodoArbol(CategoriaProducto categoriaProducto) {
+		DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+		nodo.setUserObject(categoriaProducto);
+		((DefaultTreeModel) tree.getModel()).nodeChanged(nodo);
+
+	}
+
+	private void crearEventos() {
+		tree.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent e) {
+				DefaultMutableTreeNode nodo = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+				if (nodo.getLevel() != 0) {
+					categoriaProducto = (CategoriaProducto) nodo.getUserObject();
+					lblId.setText(categoriaProducto.getId().toString());
+					txtNombre.setText(categoriaProducto.getNombre());
+					txtDependencia.setText(nodo.getParent().toString());
+					lblDepen.setText(categoriaProducto.getDependencia().toString());
+					chbxContieneProductos.setSelected(categoriaProducto.getContieneProductos());
+
+					if (nodo.getChildCount() > 0) {
+						chbxContieneProductos.setEnabled(false);
+					} else {
+						chbxContieneProductos.setEnabled(true);
+					}
+
+					System.out.println("raiz " + nodo.getParent());
+					System.out.println("Cate: " + categoriaProducto);
+					System.out.println("Cate: " + nodo.getChildCount());
+					System.out.println("nivel: " + nodo);
+					
+
+				}
+			}
+		});
+
+		chbxContieneProductos.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				if (chbxContieneProductos.isSelected() == true) {
+					btnNuevo.setEnabled(false);
+					
+				} else {
+					btnNuevo.setEnabled(true);
+				}
+			}
+		});
+		btnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				verificarUbicacionNodoNuevo();
+				limpiarcampos();
+			}
+
+		});
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (!isCamposLlenos()) {
+					JOptionPane.showMessageDialog(null, "No deje campos vacíos");
+					return;
+				}
+				if (lblId.getText().toString() == "0") {
+					CategoriaProducto categoriaProducto = new CategoriaProducto(txtNombre.getText(),
+							Integer.parseInt(lblDepen.getText()), chbxContieneProductos.isSelected());
+					CategoriaProductoController categoriaProductoController = new CategoriaProductoController();
+					categoriaProductoController.saveCategoriaProducto(categoriaProducto);
+					if (categoriaProducto.getId() != 0) {
+						agregarNodoArbol(categoriaProducto);
+						limpiarcampos();
+					}
+				} else {
+					CategoriaProducto categoriaProducto = new CategoriaProducto(Integer.parseInt(lblId.getText()),
+							txtNombre.getText(), Integer.parseInt(lblDepen.getText()),
+							chbxContieneProductos.isSelected());
+					CategoriaProductoController categoriaProductoController = new CategoriaProductoController();
+					if (categoriaProductoController.updateCategoriaProducto(categoriaProducto)) {
+						actualizarNodoArbol(categoriaProducto);
+					} else {
+						JOptionPane.showMessageDialog(null, "Error, no es posible actualizar");
+						return;
+					}
+					System.out.println(categoriaProducto);
+				}
+			}
+		});
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+	}
+
+	private void crearControles() {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 641, 336);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+
+		panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		btnNuevo = new JButton("Nuevo");
+
+		btnGuardar = new JButton("Guardar");
+
+		btnCancelar = new JButton("Cancelar");
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1
+				.setHorizontalGroup(
+						gl_panel_1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_1.createSequentialGroup().addContainerGap()
+										.addComponent(btnNuevo, GroupLayout.PREFERRED_SIZE, 122,
+												GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 113,
+												GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)
+						.addGap(43)));
+		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup().addContainerGap()
+						.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(btnNuevo, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnGuardar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE)
+						.addComponent(btnCancelar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE))
+				.addContainerGap(23, Short.MAX_VALUE)));
+		panel_1.setLayout(gl_panel_1);
+
+		panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+
+		lblId = new JLabel("0");
+		lblId.setBounds(57, 163, 16, 14);
+		lblId.setForeground(Color.DARK_GRAY);
+		lblId.setVisible(false);
+
+		chbxContieneProductos = new JCheckBox("Contiene productos");
+		chbxContieneProductos.setBounds(10, 123, 162, 23);
+		lblDepen = new JLabel("0");
+		lblDepen.setBounds(31, 163, 16, 14);
+		lblDepen.setVisible(false);
+
+		lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(10, 11, 41, 14);
+
+		lblDependencia = new JLabel("Dependencia:");
+		lblDependencia.setBounds(10, 59, 66, 14);
+
+		txtNombre = new JTextField();
+		txtNombre.setBounds(10, 28, 304, 20);
+		txtNombre.setColumns(10);
+
+		txtDependencia = new JTextField();
+		txtDependencia.setBounds(10, 88, 304, 20);
+		txtDependencia.setEditable(false);
+		txtDependencia.setColumns(10);
+		panel_2.setLayout(null);
+		panel_2.add(lblDependencia);
+		panel_2.add(lblNombre);
+		panel_2.add(lblDepen);
+		panel_2.add(lblId);
+		panel_2.add(chbxContieneProductos);
+		panel_2.add(txtNombre);
+		panel_2.add(txtDependencia);
+
+		scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.WEST);
+		scrollPane.setPreferredSize(new Dimension(200, 200));
+
+		tree = new JTree();
+		scrollPane.setViewportView(tree);
+	}
+
+}
