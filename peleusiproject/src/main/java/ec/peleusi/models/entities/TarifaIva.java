99c1fdb2864ce8178d63e02f85c046bbdfe2b4d5
Luisa Carangui <lmccarangui@gmail.com>
2016-04-07 18:31:03 -0500

    Realizado la Entidad y el Dao de Tarifa IVa

---
+++ peleusiproject/src/main/java/ec/peleusi/models/entities/TarifaIva.java
@@ -0,0 +1,76 @@
+package ec.peleusi.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table (name = "tarifaiva")
+
+public class TarifaIva {
+	
+	public TarifaIva() {
+		
+	}
+
+	public TarifaIva(String codigo, String nombre, Double porcentaje) {
+		this.id = null;
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.porcentaje = porcentaje;
+	}
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+	
+	
+	@Column(name = "codigo", unique = true, nullable = false, length = 20)
+	private String codigo;
+	
+	@Column(name = "nombre", unique = true, nullable = false, length = 50)
+	private String nombre;
+
+	@Column(name = "porcentaje", unique = true, nullable = false, length = 10)
+	private Double porcentaje;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPorcentaje() {
+		return porcentaje;
+	}
+
+	public void setPorcentaje(Double porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+	@Override
+	public String toString() {
+		return "[id=" + id + ", codigo=" + codigo + " nombre=" + nombre + "]";
+	}
+}
