cb5e0a6a21938350a97bd8e49d0925e8188be078
Luisa Carangui <lmccarangui@gmail.com>
2016-04-08 17:58:07 -0500

    Cambiado controlador de Tipo de Identificacion

--- peleusiproject/src/main/java/ec/peleusi/controllers/TipoIdentificacionController.java
+++ peleusiproject/src/main/java/ec/peleusi/controllers/TipoIdentificacionController.java
@@ -2,9 +2,8 @@ package ec.peleusi.controllers;
 
 import ec.peleusi.models.daos.DaoFactory;
 import ec.peleusi.models.daos.TipoIdentificacionDao;
-import ec.peleusi.models.daos.UnidadMedidaDao;
 import ec.peleusi.models.entities.TipoIdentificacion;
-import ec.peleusi.models.entities.UnidadMedida;
+
 
 public class TipoIdentificacionController {
 
