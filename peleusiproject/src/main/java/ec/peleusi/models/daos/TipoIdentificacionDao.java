a52f9165bc795095a9ec87389d94d74d44009450
Luisa Carangui <lmccarangui@gmail.com>
2016-04-08 17:18:00 -0500

    Realizada la Entidad y el DAO de Tipo de Identificaciòn

---
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/TipoIdentificacionDao.java
@@ -0,0 +1,14 @@
+package ec.peleusi.models.daos;
+
+import ec.peleusi.models.entities.TipoIdentificacion;;
+
+	public class TipoIdentificacionDao extends GenericDao<TipoIdentificacion, Integer> {
+
+		public TipoIdentificacionDao() {
+			super(TipoIdentificacion.class);
+
+		}
+
+	}
+
+
