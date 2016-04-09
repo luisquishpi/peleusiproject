79b8814050e9041738bc99ba416fbde940d8b3ec
LUIS QUISHPI <luis.quishpi@outlook.com>
2016-04-09 02:28:15 +0200

    Agregado Empresa a Dao y Hibernate

--- peleusiproject/src/main/java/ec/peleusi/models/daos/DaoFactory.java
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/DaoFactory.java
@@ -25,24 +25,31 @@ public class DaoFactory {
 	public CategoriaProductoDao getCategoriaProductoDao() {
 		return new CategoriaProductoDao();
 	}
+
 	public TipoGastoDeducibleDao getTipoGastoDeducibleDao() {
 		return new TipoGastoDeducibleDao();
 	}
-	/*public EmpresaDao getEmpresaDao() {
+
+	public EmpresaDao getEmpresaDao() {
 		return new EmpresaDao();
-	}*/
+	}
+
 	public TarifaIvaDao getTarifaIvaDao() {
 		return new TarifaIvaDao();
 	}
+
 	public TarifaIceDao getTarifaIceDao() {
 		return new TarifaIceDao();
 	}
+
 	public ProductoDao getProductoDao() {
 		return new ProductoDao();
 	}
+
 	public TipoIdentificacionDao getTipoIdentificacionDao() {
 		return new TipoIdentificacionDao();
 	}
+
 	public TipoPrecioDao getTipoPrecioDao() {
 		return new TipoPrecioDao();
 	}
