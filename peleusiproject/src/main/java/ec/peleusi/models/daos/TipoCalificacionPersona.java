4eebef82aca7f3573c25064a74694feb6d1418dd
Giovanni Avila <giovanniavila2011@gmail.com>
2016-04-08 20:58:23 -0500

    Actualizado Dao TipoCalificacionPersona

--- peleusiproject/src/main/java/ec/peleusi/models/daos/TipoCalificacionPersona.java
+++
@@ -1,7 +0,0 @@
-package ec.peleusi.models.daos;
-
-public class TipoCalificacionPersona extends GenericDao<TipoCalificacionPersona, Integer> {
-	public TipoCalificacionPersona() {
-		super(TipoCalificacionPersona.class);
-	}
-}
---
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/TipoCalificacionPersonaDao.java
@@ -0,0 +1,7 @@
+package ec.peleusi.models.daos;
+
+public class TipoCalificacionPersonaDao extends GenericDao<TipoCalificacionPersonaDao, Integer> {
+	public TipoCalificacionPersonaDao() {
+		super(TipoCalificacionPersonaDao.class);
+	}
+}
