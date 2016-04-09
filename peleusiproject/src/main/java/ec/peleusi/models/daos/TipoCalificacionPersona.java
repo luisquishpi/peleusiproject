5d0bd6dfc0c43997cc4dddb2b8b0ec1a03f95b12
Giovanni Avila <giovanniavila2011@gmail.com>
2016-04-08 20:53:32 -0500

    Creado Entidad y Dao del TipoCalificacionPersona

---
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/TipoCalificacionPersona.java
@@ -0,0 +1,7 @@
+package ec.peleusi.models.daos;
+
+public class TipoCalificacionPersona extends GenericDao<TipoCalificacionPersona, Integer> {
+	public TipoCalificacionPersona() {
+		super(TipoCalificacionPersona.class);
+	}
+}
