a5754b13cfe39a763b429a82cbba3ba6979a54bb
Luisa Carangui <lmccarangui@gmail.com>
2016-04-12 18:33:19 -0500

    Creado Controllador y formulario de Tipo de pago y modificaciòn del DAO y Entidad

--- peleusiproject/src/main/java/ec/peleusi/models/entities/TipoPago.java
+++ peleusiproject/src/main/java/ec/peleusi/models/entities/TipoPago.java
@@ -4,50 +4,47 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import static javax.persistence.GenerationType.IDENTITY;
-import javax.persistence.Id;
+import javax.persistence.Id; 
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tipopago")
-
 public class TipoPago {
-
-	public TipoPago() {
-
+	TipoPago(){
 	}
 
-	public TipoPago(Integer id, String nombre) {
+	public TipoPago( String nombre){
+		super();
 		this.id = null;
 		this.nombre = nombre;
 	}
-
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "id", unique = true, nullable = false)
+	@Column(name= "id", unique = true, nullable = false)
 	private Integer id;
-
+	
 	@Column(name = "nombre", unique = true, nullable = false, length = 50)
 	private String nombre;
 
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "[id=" + id + ", nombre=" + nombre + "]";
 	}
+	}
+	
+	
+	
 
-}
