e774e39fd9c920a2fa86555a6109d12e77f01a85
Giovanni Avila <giovanniavila2011@gmail.com>
2016-05-02 19:39:49 -0500

    Creado TipoRetencionListFrm

--- peleusiproject/src/main/java/ec/peleusi/controllers/TipoRetencionController.java
+++ peleusiproject/src/main/java/ec/peleusi/controllers/TipoRetencionController.java
@@ -2,9 +2,14 @@ package ec.peleusi.controllers;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import ec.peleusi.models.daos.DaoFactory;
 import ec.peleusi.models.daos.TipoRetencionDao;
 import ec.peleusi.models.entities.TipoRetencion;
+import ec.peleusi.utils.HibernateUtil;
 
 public class TipoRetencionController {
 
@@ -17,9 +22,66 @@ public class TipoRetencionController {
 	public String createTipoRetencion(TipoRetencion tipoRetencion) {
 		return tipoRetencionDao.create(tipoRetencion);
 	}
+		public boolean existTipoRetencion(TipoRetencion tipoRetencion){
+		  return tipoRetencionDao.read(tipoRetencion.getId()) !=null;
+		}
+		 
+		
+		
 	public List<TipoRetencion> tipoRetencionList() {
 		return  tipoRetencionDao.findAll();
 	}
 	
+	public TipoRetencion geTipoRetencion(int id){
+		return DaoFactory.getFactory().getTipoRetencionDao().read(id);
+	}
+	public boolean update(TipoRetencion tipoRetencion){
+		return tipoRetencionDao.update(tipoRetencion);
+	}
+	public boolean delete(Integer id){
+		return tipoRetencionDao.deleteById(id);
+	}
+	public TipoRetencion getTipoRetencion(String codigo){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try{
+			session.beginTransaction();
+			Query query = session.createQuery("from TipoRetencion T WHERE T.codigo = :codigo");
+			query.setParameter("codigo", codigo);
+			if(!query.list().isEmpty()){
+				return (TipoRetencion) query.list().get(0);
+			}
+			session.getTransaction().commit();
+		} catch(HibernateException e){
+			if(session.getTransaction() !=null)
+				e.printStackTrace();
+		}finally{
+			if (session !=null && session.isOpen()){
+				session.close();
+			}
+		}
+		return null;
+	}
+	@SuppressWarnings("unchecked")
+	public List<TipoRetencion> tipoRetencionList(String parametro){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		try{
+			
+				session.beginTransaction();
+				Query query = session.createQuery("from TipoRetencion T WHERE CONCAT(T.codigo,T.descripcion) LIKE CONCAT('%',:parametro, '%')");
+				query.setParameter("parametro", parametro);
+				if(!query.list().isEmpty()){
+					return query.list();
+				}
+				session.getTransaction().commit();
+		}catch(HibernateException e){
+			   if(session.getTransaction() !=null)
+				   e.printStackTrace();
+		}finally{
+				if(session !=null&&session.isOpen()){
+					     session.close();
+				}
+		}
+		return null;
+	}
 }
-
