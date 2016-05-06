71862d7cd362193f11e7988e5bba127204ff1e28
Luisa Carangui <lmccarangui@gmail.com>
2016-05-06 17:48:39 -0500

    Creado listado de Unidad de Medida Modificado el Controllador, dao y formulario de Unidad de Medida

--- peleusiproject/src/main/java/ec/peleusi/controllers/UnidadMedidaController.java
+++ peleusiproject/src/main/java/ec/peleusi/controllers/UnidadMedidaController.java
@@ -2,9 +2,13 @@ package ec.peleusi.controllers;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import ec.peleusi.models.daos.DaoFactory;
 import ec.peleusi.models.daos.UnidadMedidaDao;
 import ec.peleusi.models.entities.UnidadMedida;
+import ec.peleusi.utils.HibernateUtil;
 
 public class UnidadMedidaController {
 
@@ -17,8 +21,39 @@ public class UnidadMedidaController {
 	public String createUnidadMedida(UnidadMedida unidadMedida) {
 		return unidadMedidaDao.create(unidadMedida);
 	}
-	public List<UnidadMedida> unidadMedidaList() {
+
+	public List<UnidadMedida> UnidadMedidaList() {
 		return unidadMedidaDao.findAll();
 	}
 
+	public UnidadMedida getunidadMedida(int id) {
+		return DaoFactory.getFactory().getUnidadMedidaDao().read(id);
+	}
+	
+	public UnidadMedida getUnidadMedidaIdentificacion(String id) {
+		return unidadMedidaDao.getUnidadMedidaIdentificacion(id);
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UnidadMedida> UnidadMedidaList(String parametro) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery("from UnidadMedida T WHERE CONCAT(T.id,T.nombre,T.abreviatura) LIKE CONCAT('%', :parametro, '%')");
+			query.setParameter("parametro", parametro); 
+			if (!query.list().isEmpty()) {
+				return query.list();
+			}
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			if (session.getTransaction() != null)
+				e.printStackTrace();
+		} finally {
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+		return null;
+		}
 }
