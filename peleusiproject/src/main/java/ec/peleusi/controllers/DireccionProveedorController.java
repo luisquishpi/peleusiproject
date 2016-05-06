e8e65f5308a299206ee8932f2e73bab2d16e5326
Luisa Carangui <lmccarangui@gmail.com>
2016-05-05 19:14:19 -0500

    Creado proveedor controller, dao, formulario y DireccionProveedor el controller, tambièn la lista de proveedor

---
+++ peleusiproject/src/main/java/ec/peleusi/controllers/DireccionProveedorController.java
@@ -0,0 +1,37 @@
+package ec.peleusi.controllers;
+
+import ec.peleusi.models.daos.DireccionProveedorDao;
+import java.util.List;
+import ec.peleusi.models.daos.DaoFactory;
+import ec.peleusi.models.entities.DireccionProveedor;
+import ec.peleusi.models.entities.Proveedor;
+
+public class DireccionProveedorController {
+
+	private DireccionProveedorDao direccionProveedorDao;
+
+	public DireccionProveedorController() {
+		direccionProveedorDao = new DaoFactory().getDireccionProveedorDao();
+	}
+
+	public String createDireccionPersona(DireccionProveedor direccionProveedor) {
+		return direccionProveedorDao.create(direccionProveedor);
+	}
+	
+	public List<DireccionProveedor> DireccionProveedorList() {
+		return direccionProveedorDao.findAll();
+	}
+	
+	public List<DireccionProveedor> DireccionProveedorIdList(Proveedor proveedor) {
+		return direccionProveedorDao.DireccionProveedorList(proveedor);
+	}	
+		
+	public DireccionProveedor getDireccionProveedor(int id) {
+		return DaoFactory.getFactory().getDireccionProveedorDao().read(id);
+	}	
+	
+	public boolean update(DireccionProveedor direccionProveedor) {
+		return direccionProveedorDao.update(direccionProveedor);
+	}
+	
+}
--- peleusiproject/src/main/java/ec/peleusi/controllers/ProveedorController.java
+++ peleusiproject/src/main/java/ec/peleusi/controllers/ProveedorController.java
@@ -2,9 +2,13 @@ package ec.peleusi.controllers;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import ec.peleusi.models.daos.DaoFactory;
 import ec.peleusi.models.daos.ProveedorDao;
 import ec.peleusi.models.entities.Proveedor;
+import ec.peleusi.utils.HibernateUtil;
 
 public class ProveedorController {
 
@@ -24,9 +28,40 @@ public class ProveedorController {
 		return proveedorDao.findAll();
 	}
 
-	public Proveedor getPersonaIdentificacion(String identificacion) {
+	public Proveedor getProveedorIdentificacion(String identificacion) {
 		return proveedorDao.getProveedorIdentificacion(identificacion);
 
+	}
+
+	public List<Proveedor> ProveedoresList(String identificacion) {
+		return proveedorDao.findAll();
+	}
+
+	public Proveedor getProveedor(int id) {
+		return DaoFactory.getFactory().getProveedorDao().read(id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Proveedor> ProveedorList(String parametro) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery(
+					"from Proveedor T WHERE CONCAT(T.identificacion,T.razonSocial,T.tipoIdentificacion.nombre) LIKE CONCAT('%', :parametro, '%')");
+			query.setParameter("parametro", parametro);
+			if (!query.list().isEmpty()) {
+				return query.list();
+			}
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			if (session.getTransaction() != null)
+				e.printStackTrace();
+		} finally {
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+		return null;
 	}
 
 }
