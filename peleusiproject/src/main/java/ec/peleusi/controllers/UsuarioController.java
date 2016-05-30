c5c5dc3db49815f48016d978fe435fb0ee8bee53
Luisa Carangui <lmccarangui@gmail.com>
2016-05-30 18:03:11 -0500

    Modificado Controller de Usuario Realizado Eliminar y Actualizar de usuario

--- peleusiproject/src/main/java/ec/peleusi/controllers/UsuarioController.java
+++ peleusiproject/src/main/java/ec/peleusi/controllers/UsuarioController.java
@@ -16,6 +16,14 @@ public class UsuarioController {
 	public String createUsuario(Usuario usuario) {
 		return usuarioDao.create(usuario);
 	}
+	
+	public String updateUsuario(Usuario usuario) {
+		return usuarioDao.update(usuario);
+	}
+
+	public String deleteUsuario(Usuario usuario) {
+		return usuarioDao.deleteById(usuario.getId());
+	}
 
 	public List<Usuario> usuarioList() {
 		return usuarioDao.findAll();
