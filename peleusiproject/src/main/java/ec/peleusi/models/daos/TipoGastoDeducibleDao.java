5a0ddb108f1e7253633ceb304c0d699023c889d3
Luisa Carangui <lmccarangui@gmail.com>
2016-06-01 19:09:52 -0500

    Modificado Gasto Deducible Entidad, Dao, Controlller, crud Creado Lista de Gasto Deducible

--- peleusiproject/src/main/java/ec/peleusi/models/daos/TipoGastoDeducibleDao.java
+++ peleusiproject/src/main/java/ec/peleusi/models/daos/TipoGastoDeducibleDao.java
@@ -1,11 +1,39 @@
 package ec.peleusi.models.daos;
 
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import ec.peleusi.models.entities.TipoGastoDeducible;
+import ec.peleusi.utils.HibernateUtil;
 
 public class TipoGastoDeducibleDao extends GenericDao<TipoGastoDeducible, Integer> {
 
 	public TipoGastoDeducibleDao() {
 		super(TipoGastoDeducible.class);
 	}
-
+	
+	@SuppressWarnings("unchecked")
+	public List<TipoGastoDeducible> TipoGastoDeducibleList(String parametro) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery(
+					"from TipoGastoDeducible T WHERE CONCAT(T.id,T.nombre) LIKE CONCAT('%', :parametro, '%')");
+			query.setParameter("parametro", parametro);
+			if (!query.list().isEmpty()) {
+				return query.list();
+			}
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			if (session.getTransaction() != null)
+				e.printStackTrace();
+		} finally {
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+		return null;
+	}
 }
--- peleusiproject/src/main/java/ec/peleusi/views/windows/TipoGastoDeducibleCrudFrm.java
+++ peleusiproject/src/main/java/ec/peleusi/views/windows/TipoGastoDeducibleCrudFrm.java
@@ -2,12 +2,14 @@ package ec.peleusi.views.windows;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,25 +17,123 @@ import javax.swing.JTextField;
 import ec.peleusi.controllers.TipoGastoDeducibleController;
 import ec.peleusi.models.entities.TipoGastoDeducible;
 
-public class TipoGastoDeducibleCrudFrm extends JInternalFrame {
+public class TipoGastoDeducibleCrudFrm extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	private JButton btnEliminar;
 	private JButton btnGuardar;
 	private JButton btnNuevo;
 	private JButton btnCancelar;
 	private JTextField txtNombre;
+	private TipoGastoDeducible tipoGastoDeducible;
 
 	public TipoGastoDeducibleCrudFrm() {
-		setTitle("TIPOGASTODEDUCIBLE");
+		setTitle("Tipo Gasto Deducible");
 		crearControles();
 		crearEventos();
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent arg0) {
+				llenarCamposConEntidad();
+				txtNombre.requestFocus();
+				}			
+		});
 	}
 
+	private void llenarCamposConEntidad() {
+		if (tipoGastoDeducible != null && tipoGastoDeducible.getId() != null) {
+			this.setTitle("Actualizando Tipo Gasto Deducible");
+			btnGuardar.setText("Actualizar");
+			limpiarCampos();
+			txtNombre.setText(tipoGastoDeducible.getNombre());
+		} else {
+			this.setTitle("Creando Tipo Gasto Deducible");
+			btnGuardar.setText("Guardar");
+			limpiarCampos();
+		}
+	}
+	
+	private void llenarEntidadAntesDeGuardar() {
+		tipoGastoDeducible.setNombre(txtNombre.getText());
+	}
+	
+	private void guardarNuevoTipoGastoDeducible() {
+		tipoGastoDeducible= new TipoGastoDeducible();
+		llenarEntidadAntesDeGuardar();
+		TipoGastoDeducibleController tipoGastoDeducibleController = new TipoGastoDeducibleController();
+		String error = tipoGastoDeducibleController.createTipoGastoDeducible(tipoGastoDeducible);
+		if (error == null) {
+			JOptionPane.showMessageDialog(null, "Guardado correctamente", "Éxito", JOptionPane.PLAIN_MESSAGE);
+			dispose();
+		} else {
+			JOptionPane.showMessageDialog(null, error, "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	private void actualizarTipoGastoDeducible() {
+		llenarEntidadAntesDeGuardar();
+		TipoGastoDeducibleController tipoGastoDeducibleController = new TipoGastoDeducibleController();
+		String error = tipoGastoDeducibleController.updateTipoGastoDeducible(tipoGastoDeducible);
+		if (error == null) {
+			JOptionPane.showMessageDialog(null, "Actualizado correctamente", "Éxito", JOptionPane.PLAIN_MESSAGE);
+			dispose();
+		} else {
+			JOptionPane.showMessageDialog(null, error, "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public TipoGastoDeducible getTipoGastoDeducible() {
+		return tipoGastoDeducible;
+	}
+	
+	public void setTipoGastoDeducible(TipoGastoDeducible tipoGastoDeducible) {
+		this.tipoGastoDeducible = new TipoGastoDeducible();
+		this.tipoGastoDeducible = tipoGastoDeducible;
+	}
+	
+	private void limpiarCampos() {
+		txtNombre.setText("");
+		txtNombre.requestFocus();
+	}
+	
+	private boolean isCamposLlenos() {
+		boolean llenos = true;
+		if (txtNombre.getText().isEmpty())
+			llenos = false;
+		return llenos;
+	}
+	
+	private void crearEventos() {
+		btnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				tipoGastoDeducible = new TipoGastoDeducible();
+				llenarCamposConEntidad();
+			}
+		});
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!isCamposLlenos()) {
+					JOptionPane.showMessageDialog(null, "Datos incompletos, no es posible guardar", "Atenciòn", JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				if (tipoGastoDeducible != null && tipoGastoDeducible.getId() != null) {
+					actualizarTipoGastoDeducible();
+				} else {
+					guardarNuevoTipoGastoDeducible();
+				}
+
+			}
+		});
+		
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tipoGastoDeducible = new TipoGastoDeducible();
+				dispose();
+			}
+		});
+	}	
+	
 	private void crearControles() {
-		setIconifiable(true);
-		setClosable(true);
-		setBounds(100, 100, 611, 162);
+		setBounds(100, 100, 503, 197);
 
 		JPanel panelCabecera = new JPanel();
 		panelCabecera.setPreferredSize(new Dimension(200, 70));
@@ -44,88 +144,32 @@ public class TipoGastoDeducibleCrudFrm extends JInternalFrame {
 		btnNuevo = new JButton("Nuevo");
 		btnNuevo.setIcon(
 				new ImageIcon(TipoGastoDeducibleCrudFrm.class.getResource("/ec/peleusi/utils/images/new.png")));
-		btnNuevo.setBounds(10, 11, 130, 39);
+		btnNuevo.setBounds(20, 14, 130, 39);
 		panelCabecera.add(btnNuevo);
 
 		btnGuardar = new JButton("Guardar");
 		btnGuardar.setIcon(
 				new ImageIcon(TipoGastoDeducibleCrudFrm.class.getResource("/ec/peleusi/utils/images/save.png")));
-		btnGuardar.setBounds(150, 11, 130, 39);
+		btnGuardar.setBounds(180, 14, 130, 39);
 		panelCabecera.add(btnGuardar);
-
-		btnEliminar = new JButton("Eliminar");
-		btnEliminar.setIcon(
-				new ImageIcon(TipoGastoDeducibleCrudFrm.class.getResource("/ec/peleusi/utils/images/delete.png")));
-		btnEliminar.setBounds(290, 11, 130, 39);
-		panelCabecera.add(btnEliminar);
 
 		btnCancelar = new JButton("Cancelar");
 		btnCancelar.setIcon(
 				new ImageIcon(TipoGastoDeducibleCrudFrm.class.getResource("/ec/peleusi/utils/images/cancel.png")));
-		btnCancelar.setBounds(430, 11, 130, 39);
+		btnCancelar.setBounds(340, 14, 130, 39);
 		panelCabecera.add(btnCancelar);
 
 		JPanel panelCuerpo = new JPanel();
 		getContentPane().add(panelCuerpo, BorderLayout.CENTER);
 		panelCuerpo.setLayout(null);
 
-		JLabel lblNombre = new JLabel("Nombre ");
-		lblNombre.setBounds(10, 11, 117, 14);
+		JLabel lblNombre = new JLabel("Nombre *");
+		lblNombre.setBounds(31, 39, 65, 14);
 		panelCuerpo.add(lblNombre);
 
 		txtNombre = new JTextField();
-		txtNombre.setBounds(137, 8, 210, 20);
+		txtNombre.setBounds(106, 39, 214, 20);
 		panelCuerpo.add(txtNombre);
 		txtNombre.setColumns(10);
-	}
-
-	private void crearEventos() {
-		btnNuevo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				limpiarCampos();
-			}
-		});
-		btnGuardar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (!isCamposLlenos()) {
-					JOptionPane.showMessageDialog(null, "Datos incompletos, no es posible guardar", "Atenciòn", JOptionPane.WARNING_MESSAGE);
-					return;
-				}
-				TipoGastoDeducible tipogastodeducible = new TipoGastoDeducible(txtNombre.getText());
-				TipoGastoDeducibleController tipoGastoDeducibleController = new TipoGastoDeducibleController();
-				String error = tipoGastoDeducibleController.createTipoGastoDeducible(tipogastodeducible);
-				if (error == null) {
-					JOptionPane.showMessageDialog(null, "Guardado correctamente", "Éxito",
-							JOptionPane.DEFAULT_OPTION);
-					limpiarCampos();
-				} else {
-					JOptionPane.showMessageDialog(null, error, "ERROR", JOptionPane.ERROR_MESSAGE);
-				}
-
-			}
-		});
-		btnEliminar.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-	}
-
-	private void limpiarCampos() {
-		txtNombre.setText("");
-		// txtNombre.grabFocus();
-		txtNombre.requestFocus();
-	}
-
-	private boolean isCamposLlenos() {
-		boolean llenos = true;
-		if (txtNombre.getText().isEmpty())
-			llenos = false;
-		return llenos;
 	}
 }
